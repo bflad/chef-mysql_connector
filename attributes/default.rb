e71cefa085f9806c42e9d6d50c04c1508be335ff
Brian Flad <bflad417@gmail.com>
2014-01-03 00:55:40 -0500

    Fix ordering in default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 default['mysql_connector']['j']['version'] = '5.1.26'
 
-default['mysql_connector']['j']['archive_url'] = "http://cdn.mysql.com/archives/mysql-connector-java-5.1/#{node['mysql_connector']['j']['tar_file']}"
 default['mysql_connector']['j']['checksum'] =
   case node['mysql_connector']['j']['version']
   when '5.1.26' then 'fade4caa398c41f537d9b7bcaf83aabaa1eab1ef23c81de49979b5b04ba395bd'
@@ -10,4 +9,6 @@ default['mysql_connector']['j']['checksum'] =
 default['mysql_connector']['j']['install_paths'] = []
 default['mysql_connector']['j']['jar_file'] = "mysql-connector-java-#{node['mysql_connector']['j']['version']}-bin.jar"
 default['mysql_connector']['j']['tar_file'] = "mysql-connector-java-#{node['mysql_connector']['j']['version']}.tar.gz"
+
+default['mysql_connector']['j']['archive_url'] = "http://cdn.mysql.com/archives/mysql-connector-java-5.1/#{node['mysql_connector']['j']['tar_file']}"
 default['mysql_connector']['j']['url'] = "http://cdn.mysql.com/Downloads/Connector-J/#{node['mysql_connector']['j']['tar_file']}"
