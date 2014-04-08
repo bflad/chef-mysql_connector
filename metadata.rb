e5aab742c132887b1c4c785390a8e01ae09e9fbf
Brian Flad <bflad417@gmail.com>
2014-04-07 20:52:27 -0400

    Rubocop automatic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name              'mysql_connector'
-maintainer        'Brian Flad'
-maintainer_email  'bflad417@gmail.com'
-license           'Apache 2.0'
-description       'Recipes/LWRPs for installing MySQL Connectors.'
-version           '0.7.1'
-recipe            'mysql_connector', 'Empty recipe'
-recipe            'mysql_connector::j', 'Installs MySQL Connector/J via attribute.'
-recipe            'mysql_connector::odbc_package', 'Installs MySQL Connector/ODBC via package.'
+name 'mysql_connector'
+maintainer 'Brian Flad'
+maintainer_email 'bflad417@gmail.com'
+license 'Apache 2.0'
+description 'Recipes/LWRPs for installing MySQL Connectors.'
+version '0.7.1'
+recipe 'mysql_connector', 'Empty recipe'
+recipe 'mysql_connector::j', 'Installs MySQL Connector/J via attribute.'
+recipe 'mysql_connector::odbc_package', 'Installs MySQL Connector/ODBC via package.'
 
 # %w{ ark }.each do |d|
 #  depends d
