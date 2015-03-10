e68f93aeb5107ccf2488134ceb0c4b5f1518e07c
Lincoln Lee <git@lincoln.hk>
2015-03-10 23:06:13 +0800

    Update to 5.1.34

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['mysql_connector']['j']['version'] = '5.1.31'
+default['mysql_connector']['j']['version'] = '5.1.34'
 
 default['mysql_connector']['j']['checksum'] =
   case node['mysql_connector']['j']['version']
@@ -8,6 +8,9 @@ default['mysql_connector']['j']['checksum'] =
   when '5.1.29' then '04ad83b655066b626daaabb9676a00f6b4bc43f0c234cbafafac1209dcf1be73'
   when '5.1.30' then '1986baca293f998f9ecfe8a56e6e832825048a9c466cc5b5ed91940407f1210d'
   when '5.1.31' then '5a253a2aa8f06758c1901c3862402c69db601e9de456864ab1922955b225bba6'
+  when '5.1.32' then '736090d56de1e3fa97ff5f57ead504a23c88f608a0fd186dfc2565b85f95a82a'
+  when '5.1.33' then '0106bfd474bfbe64b74c019352a7025909551ab4935557f1539540d5db00b2a7'
+  when '5.1.34' then 'eb33f5e77bab05b6b27f709da3060302bf1d960fad5ddaaa68c199a72102cc5f'
   end
 default['mysql_connector']['j']['install_paths'] = []
 default['mysql_connector']['j']['jar_file'] = "mysql-connector-java-#{node['mysql_connector']['j']['version']}-bin.jar"
