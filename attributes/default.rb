b830ab1928a85f6961c79ff4a95a52e0a28cd502
David Marshall <dmarshal@yahoo-inc.com>
2015-08-07 13:17:45 -0700

    add checksum for J 5.1.35 and 5.1.36
    change default to 5.1.36

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['mysql_connector']['j']['version'] = '5.1.34'
+default['mysql_connector']['j']['version'] = '5.1.36'
 
 default['mysql_connector']['j']['checksum'] =
   case node['mysql_connector']['j']['version']
@@ -11,6 +11,8 @@ default['mysql_connector']['j']['checksum'] =
   when '5.1.32' then '736090d56de1e3fa97ff5f57ead504a23c88f608a0fd186dfc2565b85f95a82a'
   when '5.1.33' then '0106bfd474bfbe64b74c019352a7025909551ab4935557f1539540d5db00b2a7'
   when '5.1.34' then 'eb33f5e77bab05b6b27f709da3060302bf1d960fad5ddaaa68c199a72102cc5f'
+  when '5.1.35' then 'fa77909a62363c1aa49ef5ab537ed4affe90a4a6aa5dd1045555b16b2611306d'
+  when '5.1.36' then '2e8ae92c68e719732ff5d75dc7e50f2d893d5e8efd65e5c0acb372291a8fd114'
   end
 default['mysql_connector']['j']['install_paths'] = []
 default['mysql_connector']['j']['jar_file'] = "mysql-connector-java-#{node['mysql_connector']['j']['version']}-bin.jar"
