5472de01f8a05b9d7a4894e904760614f6debaf0
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-08-10 01:58:33 -0700

    Fix rubocop errors.

--- providers/j.rb
+++ providers/j.rb
@@ -1,5 +1,5 @@
 action :create do
-  ark "mysql-connector-java" do
+  ark 'mysql-connector-java' do
     url node['mysql_connector']['j']['url']
     checksum node['mysql_connector']['j']['checksum']
     creates node['mysql_connector']['j']['jar_file']
